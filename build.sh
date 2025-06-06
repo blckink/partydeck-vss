27d1dbd5659f12709e20a1f856fe0e11e35c76c5
blckink <86264410+blckink@users.noreply.github.com>
2025-06-06 10:02:12 +0200

    Update build.sh
    added horizontal / vertical kwin files

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ cargo build --release && \
 rm -rf build/partydeck-rs
 mkdir -p build/ build/res && \
 cp target/release/partydeck-rs res/PartyDeckKWinLaunch.sh build/ && \
-cp res/splitscreen_kwin.js build/res
+cp res/splitscreen_horizontal_kwin.js build/res && \
+cp res/splitscreen_vertical_kwin.js build/res
